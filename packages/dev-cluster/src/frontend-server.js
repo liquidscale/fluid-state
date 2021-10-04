a90308ebf02d38f0fef4c5fcc6fa4e782c3fb1e2
Joel Grenon <joelgrenon@gmail.com>
2021-10-04 15:52:00 -0400

    add license text everywhere

--- packages/dev-cluster/src/frontend-server.js
+++ packages/dev-cluster/src/frontend-server.js
@@ -1,3 +1,26 @@
+/**
+   MIT License
+
+   Copyright (c) 2021 Joel Grenon
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+ */
 const { createServer } = require("vite");
 const fs = require("fs-extra");
 const Path = require("path");
@@ -49,8 +72,8 @@ module.exports = function (cluster) {
       state.server = await createServer({
         root: state.tmpWebRoot,
         server: {
-          port: config.port || 5000,
-        },
+          port: config.port || 5000
+        }
       });
 
       console.log("frontend server is listening at http://localhost:5000/");
